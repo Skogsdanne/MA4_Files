bc0cccd336af4cb51d615992357cb5fdd28f9413
Skogsdanne <d.nilsson1998@gmail.com>
2023-10-04 10:33:00 +0200

    bu

--- MA4_2.py
+++ MA4_2.py
@@ -3,10 +3,12 @@
 from person import Person
 
 def main():
-	f = Person(5)
+	f = Person(50)
+	print(f.getAge())
+	print(f.getDecades())
+	f.setAge(51)
 	print(f.get())
-	f.set(7)
-	print(f.get())
+	print(f.getDecades())
 
 if __name__ == '__main__':
 	main()
